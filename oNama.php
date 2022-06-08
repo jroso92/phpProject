3b21c6e8deb0e6b746eef55150123e07377ce9a6
Josip Roso <josip.roso@kodelab.co>
2022-06-08 22:44:28 +0200

    CHANGED - page "O name"

--- oNama.php
+++ oNama.php
@@ -1,4 +1,9 @@
 <?php
+
+$url = 'https://persis-api.kodelab.co/api/persis/organisation/org1';
+$data = file_get_contents($url);
+$result = json_decode($data, true);
+
 print'
 	<h1>O nama</h1>
 		<h2>Fight site, najbolji hrvatski site o borilačkim sportovima!</h2>
@@ -6,9 +11,9 @@ print'
 		  <source src="video/mma_highlights.mp4" type="video/mp4">
 		  Your browser does not support HTML5 video.
 		</video>
-		
-	<p>Fight site je specijalizirana stranica o borilačkom sportovima i fitness-u, te pravilnoj prehrani u sportu. Objavljujemo vjesti o pravilnoj sportskoj prehrani, raznim dijetama te iskustva boraca i savjete nutricionista. Sve što vam je potrebno za uspješan trening možete pronaći na jednom mjestu, na vašem najdražem mjestu Fight site-u.</p>
-	<p>Fight site kao news portal, među europskim je pionirima on line izvještavanja sa borilačkih priredaba diljem svijeta. Pravovremene i relevantne vijesti naših najjačih novinara borilačke tematike, još su jedan dragulj na kruni Fight site projekta.</p>
+		<p><b>Naziv tvrtke</b>: '. $result["name"] .'</p>
+		<p><b>Opis</b>: '. $result["description"] .'</p>
+		<p><b>OIB</b>: '. $result["oib"] .'</p>
 	</main><br>
 		<p>Social media:<br>
 			<a href="https://www.facebook.com/Fight-Site-206239519413221/" target="_blank"><img src="img/facebook.png" alt="Facebook" title="Facebook" style="width:24px; margin-top:0.4em"></a>
